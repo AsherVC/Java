46fc37b8fe63063db7a1623192b5dfe5ac5b559c
AsherVC <89779612+AsherVC@users.noreply.github.com>
2022-11-19 09:50:28 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class input {
+    public static void main(String[] args) {
+    Scanner scanner=new Scanner(System.in);
+    System.out.print("Enter the no:");
+    int a=scanner.nextInt();
+    System.out.println("No is:"+a);
+        
+    }
+}
